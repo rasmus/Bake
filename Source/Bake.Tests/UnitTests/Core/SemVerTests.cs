61b2aafb50d45f38ac731407c9da70807af224ab
Rasmus Mikkelsen <r@smus.nu>
2021-01-17 12:22:21 +0100

    SemVer parse

---
+++ Source/Bake.Tests/UnitTests/Core/SemVerTests.cs
@@ -0,0 +1,38 @@
+﻿using Bake.Core;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Bake.Tests.UnitTests.Core
+{
+    public class SemVerTests
+    {
+        [TestCase(
+            "1.2",
+            1,
+            2,
+            0,
+            null)]
+        [TestCase(
+            "1.2.3-meta",
+            1,
+            2,
+            3,
+            "meta")]
+        public void ValidVersions(
+            string str,
+            int expectedMajor,
+            int expectedMinor,
+            int expectedPatch,
+            string expectedMeta)
+        {
+            // Act
+            var version = SemVer.Parse(str);
+
+            // Assert
+            version.Major.Should().Be(expectedMajor);
+            version.Minor.Should().Be(expectedMinor);
+            version.Patch.Should().Be(expectedPatch);
+            version.Meta.Should().Be(expectedMeta ?? string.Empty);
+        }
+    }
+}
--- Source/Bake/Core/SemVer.cs
+++ Source/Bake/Core/SemVer.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Bake.Core
 {
     public class SemVer
     {
+        private static readonly Regex VersionParser = new Regex(
+            @"^(?<major>\d+)\.(?<minor>\d+)(\.(?<patch>\d)){0,1}(\-(?<meta>[a-z0-9\-_]+)){0,1}$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
         private static readonly Random R = new Random();
 
         public static SemVer Random => new SemVer(
@@ -14,6 +18,51 @@ namespace Bake.Core
 
         public static SemVer Parse(string str)
         {
+            var exception = InternalTryParse(str, out var version);
+            if (exception != null)
+            {
+                throw exception;
+            }
+
+            return version;
+        }
+
+        public static bool TryParse(string str, out SemVer version)
+        {
+            return InternalTryParse(str, out version) == null;
+        }
+
+        public static Exception InternalTryParse(
+            string str,
+            out SemVer version)
+        {
+            version = null;
+            if (string.IsNullOrEmpty(str))
+            {
+                return new ArgumentNullException(nameof(str));
+            }
+
+            var match = VersionParser.Match(str);
+            if (!match.Success)
+            {
+                return new ArgumentException($"'{str}' is not a valid version string");
+            }
+
+            var major = int.Parse(match.Groups["major"].Value);
+            var minor = int.Parse(match.Groups["minor"].Value);
+            var patch = match.Groups["patch"].Success
+                ? int.Parse(match.Groups["patch"].Value)
+                : 0;
+            var meta = match.Groups["meta"].Success
+                ? match.Groups["meta"].Value
+                : string.Empty;
+
+            version = new SemVer(
+                major,
+                minor,
+                patch,
+                meta);
+
             return null;
         }
 
@@ -34,7 +83,7 @@ namespace Bake.Core
         private SemVer(
             int major,
             int minor,
-            int patch,
+            int patch ,
             string meta)
         {
             Major = major;
