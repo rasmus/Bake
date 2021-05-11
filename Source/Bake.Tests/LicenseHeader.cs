7e2f07e97f90e36a48980e484ac946cf6de96715
Rasmus Mikkelsen <r@smus.nu>
2021-05-11 21:44:06 +0200

    Add license

--- Source/Bake.Tests/LicenseHeader.cs
+++ Source/Bake.Tests/LicenseHeader.cs
@@ -1,8 +1,32 @@
-﻿using System;
+// MIT License
+// 
+// Copyright (c) 2021 Rasmus Mikkelsen
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using Bake.Tests.Helpers;
 using NUnit.Framework;
@@ -12,18 +36,41 @@ namespace Bake.Tests
     public class LicenseHeader : TestIt
     {
         private static readonly Regex LicenseHeaderFinder = new Regex(
-            @"^\s+\/\*.*?\*\/",
+            @"^\s*\/\/ MIT License.+(?:\n\s*\/\/.*)+\s*",
             RegexOptions.Compiled);
 
-        [Test]
+        [Test, Explicit]
         public async Task UpdateHeaders()
         {
             // Arrange
-            var currentHeader = ReadEmbeddedAsync("license.txt");
+            var currentHeader = await ReadEmbeddedAsync("license.txt");
 
-            // 
+            // Act
 
             var files = GetFiles().ToList();
+            await Task.WhenAll(files.Select(f => UpdateHeaderAsync(f, currentHeader, CancellationToken.None)));
+        }
+
+        private static async Task UpdateHeaderAsync(
+            string path,
+            string licenseHeader,
+            CancellationToken cancellationToken)
+        {
+            var content = await File.ReadAllTextAsync(path, cancellationToken);
+
+            if (LicenseHeaderFinder.IsMatch(content))
+            {
+                content = LicenseHeaderFinder.Replace(content, licenseHeader);
+            }
+            else
+            {
+                content = new StringBuilder()
+                    .Append(licenseHeader)
+                    .AppendLine(content.Trim())
+                    .ToString();
+            }
+
+            await File.WriteAllTextAsync(path, content, cancellationToken);
         }
 
         private static IEnumerable<string> GetFiles()
