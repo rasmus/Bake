ab9dad33075371c1d475628933eaf1901f414a1d
Rasmus Mikkelsen <r@smus.nu>
2021-05-27 19:33:18 +0200

    Added missing directory

---
+++ Source/Bake/ValueObjects/Artifacts/ArtifactKey.cs
@@ -0,0 +1,50 @@
+// MIT License
+// 
+// Copyright (c) 2021 Rasmus Mikkelsen
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace Bake.ValueObjects.Artifacts
+{
+    public class ArtifactKey : ValueObject
+    {
+        public string Type { get; [Obsolete] set; }
+        public string Name { get; [Obsolete] set; }
+
+        private static readonly Regex Parser = new Regex(
+            @"^(?<type>[a-z\-0-9]{1,}:(?<name>[a-z0-9\.\-])}{1,})$",
+            RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+
+        [Obsolete]
+        public ArtifactKey() { }
+
+        public ArtifactKey(
+            string type,
+            string name)
+        {
+#pragma warning disable CS0612 // Type or member is obsolete
+            Type = type;
+            Name = name;
+#pragma warning restore CS0612 // Type or member is obsolete
+        }
+    }
+}
