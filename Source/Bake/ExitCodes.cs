543155cb7e3ed4b3d578ff1618cba096edb1b8c1
Rasmus Mikkelsen <r@smus.nu>
2021-07-08 22:24:59 +0200

    Use positive exit codes

--- Source/Bake/ExitCodes.cs
+++ Source/Bake/ExitCodes.cs
@@ -30,20 +30,20 @@ namespace Bake
 
         public static class Core
         {
-            public const int UnexpectedError = -256;
+            public const int UnexpectedError = 256;
 
-            public const int NoRecipes = -3;
-            public const int CookingFailed = -3;
+            public const int NoRecipes = 3;
+            public const int CookingFailed = 4;
         }
 
         public static class Plan
         {
-            public const int PlanFileAlreadyExists = -1;
+            public const int PlanFileAlreadyExists = 1;
         }
 
         public static class Apply
         {
-            public const int PlanFileNotFound = -2;
+            public const int PlanFileNotFound = 2;
         }
     }
 }
