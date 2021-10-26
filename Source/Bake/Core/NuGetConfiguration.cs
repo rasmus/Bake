aeb1c5355d34da4ab69791dae1098edd4f8e8f3f
Rasmus Mikkelsen <r@smus.nu>
2021-10-26 14:01:47 +0200

    Cleanup

--- Source/Bake/Core/NuGetConfiguration.cs
+++ Source/Bake/Core/NuGetConfiguration.cs
@@ -99,33 +99,3 @@ namespace Bake.Core
         }
     }
 }
-
-/*
-
-<?xml version="1.0" encoding="utf-8"?>
-<configuration>
-    <packageSources>
-        <clear />
-        <add key="github" value="https://nuget.pkg.github.com/OWNER/index.json" />
-    </packageSources>
-    <packageSourceCredentials>
-        <github>
-            <add key="Username" value="USERNAME" />
-            <add key="ClearTextPassword" value="TOKEN" />
-        </github>
-    </packageSourceCredentials>
-</configuration>
-
-
-XNamespace xns = "http://www.fruitauthority.fake";
-XDocument xDoc = 
-    new XDocument(new XDeclaration("1.0", "utf-8", "yes"),
-        new XElement(xns + "FruitBasket",
-            new XElement(xns + "Fruit",
-                new XElement(xns + "FruitName", "Banana"),
-                new XElement(xns + "FruitColor", "Yellow")),
-            new XElement(xns + "Fruit",
-                new XElement(xns + "FruitName", "Apple"),
-                new XElement(xns + "FruitColor", "Red"))
-                ));
- */
