65a1d01c12745f2ad772434fc3f4b9698394e116
Rasmus Mikkelsen <r@smus.nu>
2022-02-15 21:54:08 +0100

    Update license header (#68)

--- Source/Bake/Core/Defaults.cs
+++ Source/Bake/Core/Defaults.cs
@@ -1,6 +1,6 @@
 // MIT License
 // 
-// Copyright (c) 2021 Rasmus Mikkelsen
+// Copyright (c) 2021-2022 Rasmus Mikkelsen
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -19,8 +19,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-
-// ReSharper disable StringLiteralTypo
 
 using System.Collections.Generic;
 using System.Threading;
