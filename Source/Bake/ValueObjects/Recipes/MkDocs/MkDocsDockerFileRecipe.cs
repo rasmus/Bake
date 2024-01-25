a7246153e09409f0a4eaa76483bc41b4daf4bb06
Rasmus Mikkelsen <r@smus.nu>
2024-01-25 16:33:24 +0100

    Fix compile error

--- Source/Bake/ValueObjects/Recipes/MkDocs/MkDocsDockerFileRecipe.cs
+++ Source/Bake/ValueObjects/Recipes/MkDocs/MkDocsDockerFileRecipe.cs
@@ -20,8 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-using System;
-using System.Collections.Generic;
 using Bake.ValueObjects.Artifacts;
 using YamlDotNet.Serialization;
 
@@ -31,10 +29,10 @@ namespace Bake.ValueObjects.Recipes.MkDocs
     public class MkDocsDockerFileRecipe : Recipe
     {
         [YamlMember]
-        public string Directory { get; [Obsolete] set; }
+        public string Directory { get; [Obsolete] set; } = null!;
 
         [YamlMember]
-        public Dictionary<string, string> Labels { get; [Obsolete] set; }
+        public Dictionary<string, string> Labels { get; [Obsolete] set; } = null!;
 
         [Obsolete]
         public MkDocsDockerFileRecipe() { }
